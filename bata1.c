1a65bb6a3e5207d12bebfd69f79ff6750b36d120
Trithep Ratanapipob <s6070048@kmitl.ac.th>
2014-01-02 15:54:19 +0700

    add Function cleardis printdis

--- bata1.c
+++ bata1.c
@@ -13,42 +13,83 @@ int main()
 		system("cls");
 		for(m=0;m<roll;m++)
 		{	
-			for(j=0;j<column;j++)
-			{
-				display[m][j]=' ';
-			}
+			cleardis(display);
 
 			home(display,c,m);
 
-			for(j=0;j<=column;j++)
-			{
-				printf("%c",display[m][j]);
-			}
+			printdis(display,m);
+
 			printf("\n");
-		}
+		}	
+		if(c<18)
+			break;
 	}
 }
-void home(char display[29][78],int c,int m)
+
+void cleardis(char display[roll][column])
+{
+	int m,j;
+	for(m=0;m<roll;m++)
+	{	
+		for(j=0;j<column;j++)
+		{
+			display[m][j]=' ';
+		}
+	}
+
+}
+
+void printdis(char display[roll][column],int m)
+{
+	int j;	
+	for(j=0;j<column;j++)
+	{
+		printf("%c",display[m][j]);
+	}
+}
+
+void home(char display[roll][column],int c,int m)
 {
 	int i,count =0;
-	char home[6][45]={	{"   _________       _||____      ____         "},
-						{"__/---------\\_____/-||----\\____/----\\____    "},
-						{" /___________\\   /_________\\  /______\\   \\__ "},
-						{"/|           |\\ /|         |\\/|      |\\     |"},
-						{" |           |   |         |  |      |      |"},
-						{" |___________|   |_________|  |______|      |"}};
+	char home[6][45]={	{"|     _________       _||____      ____     |"},
+						{"|    /---------\\     /-||----\\    /----\\    |"},
+						{"|   /___________\\   /_________\\  /______\\   |"},
+						{"|  /|           |\\ /|         |\\/|      |\\  |"},
+						{"|   |    Yok    |   |   kak   |  |  sus |   |"},
+						{"|   |___________|   |_________|  |______|   |"}},
+		
+		start[5][45]={	{"                    Start                    "},
+						{"                                             "},
+						{"                    Option                   "},
+						{"                                             "},
+						{"                    Exit                     "}};
 	for(i=c;i<=column;i++)
 	{
-		if(m>10&&m<17)
+		if(m>=7&&m<13)
 		{
-			display[m][i] = home[m-11][count++];
 			if(count==45)
 			{
 				break;
 			}
+			display[m][i] = home[m-7][count];
 		}
+		if(m>=17&&m<22)
+		{
+			if(count==45)
+			{
+				break;
+			}
+			display[m][i] = start[m-17][count];
+		}
+		count += 1;
+
 	}
 }
+
+// void interface(char display[roll][column],int c,int m)
+// {
+
+// }
 
 // {"   _________       _||____      ____         "},
 // {"__/---------\_____/-||----\____/----\____    "},
